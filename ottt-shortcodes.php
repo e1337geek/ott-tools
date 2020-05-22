b918810d93658092fa19579c0faaee1d4c7b6fc4
e1337geek <e1337geek@gmail.com>
2020-05-22 16:07:36 -0700

    added template file for ottt_enroll_customer shortcode

--- ottt-shortcodes.php
+++ ottt-shortcodes.php
@@ -2,6 +2,7 @@
 
 function ottt_enroll_customer_shortcode( $atts ) {
     ob_start;
+    include plugin_dir_path( __FILE__ ) . 'templates/ottt-enroll-customer.php';
     return ob_get_clean();
 }
 add_shortcode( 'ottt_enroll_customer', 'ottt_enroll_customer_shortcode' );
