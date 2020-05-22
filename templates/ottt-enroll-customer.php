3cd47fc40280abf1634549f68651aad86118ff2a
e1337geek <e1337geek@gmail.com>
2020-05-22 16:47:12 -0700

    added handling of GET params

--- templates/ottt-enroll-customer.php
+++ templates/ottt-enroll-customer.php
@@ -1,9 +1,20 @@
+<?php
+
+$customer_fname = isset( $_GET['fname'] ) ? $_GET['fname'] : '';
+$customer_lname = isset( $_GET['lname'] ) ? $_GET['lname'] : '';
+$customer_email = isset( $_GET['email'] ) ? $_GET['email'] : '';
+
+?>
+
 <div class="ottt-container enroll-customer">
     <form class="ottt-form">
-        <label for="name"><?php _e('Name', 'ottt-tools'); ?></label><br />
-        <input type="text" id="name" name="name"><br />
+        <label for="fname"><?php _e('First Name', 'ottt-tools'); ?></label><br />
+        <input type="text" id="fname" name="fname" value="<?php echo $customer_fname; ?>"><br />
 
-        <?php if(!employer): ?>
+        <label for="lname"><?php _e('Last Name', 'ottt-tools'); ?></label><br />
+        <input type="text" id="lname" name="lname" value="<?php echo $customer_lname; ?>"><br />
+
+        <?php if(!$customer_employer): ?>
             <label for="employer"><?php _e('Employer', 'ottt-tools'); ?></label><br />
             <input type="text" id="employer" name="employer"><br />
         <?php else: ?>
@@ -11,7 +22,7 @@
         <?php endif; ?>
 
         <label for="email"><?php _e('Email', 'ottt-tools'); ?></label><br />
-        <input type="text" id="email" name="email"><br />
+        <input type="text" id="email" name="email" value="<?php echo $customer_email; ?>"><br />
 
         <label for="password"><?php _e('Password', 'ottt-tools'); ?></label><br />
         <input type="text" id="password" name="password"><br />
