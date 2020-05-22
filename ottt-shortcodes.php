23ed1d0cfed8cced35d0487341244f03a706d44e
e1337geek <e1337geek@gmail.com>
2020-05-22 16:17:34 -0700

    added shortcode attribute handling

--- ottt-shortcodes.php
+++ ottt-shortcodes.php
@@ -1,8 +1,13 @@
 <?php
 
 function ottt_enroll_customer_shortcode( $atts ) {
+    extract(shortcode_atts(array(
+        'employer' => null,
+        'expiration' => null,
+    ), $atts));
+
     ob_start;
-    include plugin_dir_path( __FILE__ ) . 'templates/ottt-enroll-customer.php';
+    include (locate_template('ottt-enroll-customer.php'));
     return ob_get_clean();
 }
 add_shortcode( 'ottt_enroll_customer', 'ottt_enroll_customer_shortcode' );
