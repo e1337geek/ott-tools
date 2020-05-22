2e39123def8ead0d7bf80672a6f8c0a11d81093b
Brady Friedrich <brady@byteitsystems.com>
2020-05-22 15:56:22 -0700

    added ott-tools file with WordPress comment

---
+++ ott-tools.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: ByteIT OTT Tools
+ * Description: A set of tools to assist in integrating with the Vimeo OTT API
+ * Version: 0.1.1
+ * Author: ByteIT
+ * Author URI: https://byteitsystems.com
+ */
