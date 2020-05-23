ab89802c62391f9869e4dc31e3fa942d2ebbafb4
e1337geek <e1337geek@gmail.com>
2020-05-22 18:24:54 -0700

    added ott api call to form handler

--- ottt-shortcodes.php
+++ ottt-shortcodes.php
@@ -11,3 +11,28 @@ function ottt_enroll_customer_shortcode( $atts ) {
     return ob_get_clean();
 }
 add_shortcode( 'ottt_enroll_customer', 'ottt_enroll_customer_shortcode' );
+
+function ottt_enroll_customer_form_handler() {
+
+    /* Initialize Variables */
+    $fname = sanitize_text_field( $_POST['fname'] );
+    $lname = sanitize_text_field( $_POST['lname'] );
+    $employer = sanitize_text_field( $_POST['employer'] );
+    $email = sanitize_email( $_POST['email'] );
+    $password = $_POST['password'];
+
+    $ott_response = wp_remote_post( $url, array(
+        'method' => 'POST',
+        'headers' => array(
+            'Authorization' => 'Basic ' . base64_encode( get_option( 'ottt_api_key' ) ),
+        ),
+        'body' => array(
+            'name' => $fname . ' ' . $lname,
+            'email' => $email,
+            'password' => $password
+            'product' => 'https://api.vhx.tv/products/' . get_option( 'ottt_product_id' );
+        ),
+    ));
+}
+add_action( 'admin_post_nopriv_ottt_enroll_customer', 'ottt_enroll_customer_form_handler' );
+add_action( 'admin_post_ottt_enroll_customer', 'ottt_enroll_customer_form_handler' );
