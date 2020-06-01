545c875bb05596072b0142818785bf575660407d
e1337geek <e1337geek@gmail.com>
2020-06-01 12:35:10 -0700

    wrapped label/input pairs in divs with column classes

--- templates/ottt-enroll-customer.php
+++ templates/ottt-enroll-customer.php
@@ -28,24 +28,34 @@ $customer_email = isset( $_GET['email'] ) ? $_GET['email'] : '';
         <?php endif; ?>
 
         <form class="ottt-form" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
-            <label for="fname"><?php _e('First Name', 'ott-tools'); ?></label>
-            <input type="text" id="fname" name="fname" value="<?php echo $customer_fname; ?>"<?php echo(!$customer_fname ?: 'readonly'); ?>>
+            <div class="halfcol">
+                <label for="fname"><?php _e('First Name', 'ott-tools'); ?></label>
+                <input type="text" id="fname" name="fname" value="<?php echo $customer_fname; ?>"<?php echo(!$customer_fname ?: 'readonly'); ?>>
+            </div>
 
-            <label for="lname"><?php _e('Last Name', 'ott-tools'); ?></label>
-            <input type="text" id="lname" name="lname" value="<?php echo $customer_lname; ?>"<?php echo(!$customer_lname ?: 'readonly'); ?>>
+            <div class="halfcol">
+                <label for="lname"><?php _e('Last Name', 'ott-tools'); ?></label>
+                <input type="text" id="lname" name="lname" value="<?php echo $customer_lname; ?>"<?php echo(!$customer_lname ?: 'readonly'); ?>>
+            </div>
 
             <?php if(!$employer): ?>
-                <label for="employer"><?php _e('Employer', 'ott-tools'); ?></label>
-                <input type="text" id="employer" name="employer">
+                <div class="fullcol">
+                    <label for="employer"><?php _e('Employer', 'ott-tools'); ?></label>
+                    <input type="text" id="employer" name="employer">
+                </div>
             <?php else: ?>
                 <input type="hidden" id="employer" name="employer" value="<?php echo $employer; ?>">
             <?php endif; ?>
 
-            <label for="email"><?php _e('Email', 'ott-tools'); ?></label>
-            <input type="text" id="email" name="email" value="<?php echo $customer_email; ?>"<?php echo(!$customer_email ?: 'readonly'); ?>>
+            <div class="fullcol">
+                <label for="email"><?php _e('Email', 'ott-tools'); ?></label>
+                <input type="text" id="email" name="email" value="<?php echo $customer_email; ?>"<?php echo(!$customer_email ?: 'readonly'); ?>>
+            </div>
 
-            <label for="password"><?php _e('Password', 'ott-tools'); ?></label>
-            <input type="password" id="password" name="password">
+            <div class="fullcol">
+                <label for="password"><?php _e('Password', 'ott-tools'); ?></label>
+                <input type="password" id="password" name="password">
+            </div>
             
             <?php if( $expiration ): ?>
                 <input type="hidden" id="expiration" name="expiration" value="<?php echo $expiration; ?>">
