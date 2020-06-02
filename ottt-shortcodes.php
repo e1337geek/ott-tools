614a207c0caab54553ed81ce19c6a701402e260c
e1337geek <e1337geek@gmail.com>
2020-06-01 18:56:07 -0700

    made form labels authorable via shortcode atts

--- ottt-shortcodes.php
+++ ottt-shortcodes.php
@@ -5,11 +5,23 @@ function ottt_enroll_customer_shortcode( $atts ) {
         array(
             'employer' => null,
             'expiration' => null,
+            'label_fname' => __( 'First Name', 'ott-tools' ),
+            'label_lname' => __( 'Last Name', 'ott-tools' ),
+            'label_employer' => __( 'Employer', 'ott-tools' ),
+            'label_email' => __( 'Email Address', 'ott-tools' ),
+            'label_password' => __( 'Password', 'ott-tools' ),
+            'label_submit' => __( 'Submit', 'ott-tools' ),
         ), 
         $atts
     );
     $employer = $args['employer'];
     $expiration = $args['expiration'];
+    $label_fname = $args['label_fname'];
+    $label_lname = $args['label_lname'];
+    $label_employer = $args['label_employer'];
+    $label_email = $args['label_email'];
+    $label_password = $args['label_password'];
+    $label_submit = $args['label_submit'];
 
     ob_start();
     include ( plugin_dir_path( __FILE__ ) . 'templates/ottt-enroll-customer.php' );
