11bb375b62ae5914cd9a989dfc68c2ff36cd66d6
e1337geek <e1337geek@gmail.com>
2020-10-18 17:54:10 -0700

    troubleshooting...

--- ottt-handlers.php
+++ ottt-handlers.php
@@ -225,7 +225,7 @@ function ottt_disable_inactive_form_handler() {
 
         $disableResult = ottt_disable_customer( $customer );
         if( $disableResult['response']['code'] === 200 || $disableResult['response']['code'] === 201 ) {
-            $sqlUpdateDisabled = "UPDATE `$customerTable` SET `ottt_customer_disabled` = $currentTimestamp WHERE `ottt_customer_email` = '$customer['ottt_customer_email']'";
+            $sqlUpdateDisabled = "UPDATE `$customerTable` SET `ottt_customer_disabled` = $currentTimestamp WHERE `ottt_customer_email` = '$customer["ottt_customer_email"]'";
             require_once ABSPATH . 'wp-admin/includes/upgrade.php';
             dbDelta( $sqlUpdateDisabled );
         }
